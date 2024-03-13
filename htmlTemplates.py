c7557dd19d6fe89e4c8e3f0705b3ba319f21d2b0
Pejman <pejman.ebrahimi77@gmail.com>
2024-03-13 19:13:50 +0100

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #0096FF
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://scholar.googleusercontent.com/citations?view_op=medium_photo&user=zO-nHd0AAAAJ&citpid=8" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://static1.s123-cdn-static-a.com/uploads/8792113/400_filter_nobg_6582fc18c112d.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
